4562f43ae5d0c1d5a2bea20b78a59e554e9f7d5d
guoce <hfpgce@gmail.com>
2024-04-11 22:26:27 +0800

    Update main.cpp

--- wfw/main.cpp
+++ wfw/main.cpp
@@ -2,11 +2,13 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
-void init(GLFWwindow* window){}
-
-void display(GLFWwindow* window,double currentTime)
+void init(GLFWwindow* window)
 {
-	glClearColor(1.0,0.0,0.0,1.0);
+}
+
+void display(GLFWwindow* window, double currentTime)
+{
+	glClearColor(1.0, 0.0, 0.0, 1.0);
 	glClear(GL_COLOR_BUFFER_BIT);
 }
 
@@ -17,13 +19,13 @@ int main()
 		exit(EXIT_FAILURE);
 	}
 
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR,4);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR,4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 
-	GLFWwindow* window=glfwCreateWindow(600,600,"chaper2 - program1",NULL,NULL);
+	GLFWwindow* window = glfwCreateWindow(600, 600, "chaper2 - program1",NULL,NULL);
 	glfwMakeContextCurrent(window);
 
-	if (glewInit()!=GLEW_OK)
+	if (glewInit() != GLEW_OK)
 	{
 		exit(EXIT_FAILURE);
 	}
@@ -34,7 +36,7 @@ int main()
 
 	while (!glfwWindowShouldClose(window))
 	{
-		display(window,glfwGetTime());
+		display(window, glfwGetTime());
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
